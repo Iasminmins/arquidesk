2c76e9ae60fbb05f5636eeaef67192e86e0587dd
Iasmin <iasminbabaloo34@gmail.com>
2026-06-23 12:21:34 -0300

    Fix Neon connection and ignore build artifacts

--- src/ArquiDesk.Web/Program.cs
+++ src/ArquiDesk.Web/Program.cs
@@ -114,8 +114,15 @@ static string GetPostgresConnectionString(IConfiguration configuration)
     var uri = new Uri(connectionString);
     var userInfo = uri.UserInfo.Split(':', 2);
     var username = Uri.UnescapeDataString(userInfo[0]);
-    var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : string.Empty;
+    var password = userInfo.Length > 1
+        ? Uri.UnescapeDataString(userInfo[1])
+        : string.Empty;
+
     var database = uri.AbsolutePath.TrimStart('/');
 
-    return $"Host={uri.Host};Port={uri.Port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true;Pooling=true";
+    var port = uri.Port > 0
+        ? uri.Port
+        : 5432;
+
+    return $"Host={uri.Host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true;Pooling=true";
 }
